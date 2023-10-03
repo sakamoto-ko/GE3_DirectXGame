8f26ae9f5e8a2ee14eaef452ad9a6e223cfa66ff
sakamoto-ko <115054832+sakamoto-ko@users.noreply.github.com>
2023-10-03 17:17:49 +0900

    02_03_確認課題

--- Input.cpp
+++ Input.cpp
@@ -10,8 +10,10 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd) {
 
     HRESULT result;
 
+    //前回のキー入力を保存
+    memcpy(keyPre, key, sizeof(key));
+
     // DirectInputのインスタンス生成
-    ComPtr<IDirectInput8> dInput = nullptr;
     result = DirectInput8Create(
         hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&dInput, nullptr);
     assert(SUCCEEDED(result));
@@ -26,15 +28,34 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd) {
     result = keyboard->SetCooperativeLevel(
         hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
     assert(SUCCEEDED(result));
-
 }
+
 //更新
 void Input::Update() {
 
     // キーボード情報の取得開始
     keyboard->Acquire();
     // 全キーの入力状態を取得する
-    BYTE key[256] = {};
     keyboard->GetDeviceState(sizeof(key), key);
+}
 
+bool Input::PushKey(BYTE keyNumber) {
+
+    //指定キーを押していればtrueを返す
+    if (key[keyNumber]) {
+        return true;
+    }
+    //そうでなければfalseを返す
+    return false;
+}
+
+bool Input::TriggerKey(BYTE keyNumber) {
+
+    //指定キーをトリガーしていればtrueを返す
+    if (key[keyNumber] &&
+        keyPre[keyNumber] != 1) {
+        return true;
+    }
+    //そうでなければfalseを返す
+    return false;
 }
--- Input.h
+++ Input.h
@@ -12,13 +12,37 @@ class Input
 {
 public://namespace省略
 	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
 public://メンバ関数
 	//初期化
 	void Initialize(HINSTANCE hInstance, HWND hwnd);
 	//更新
 	void Update();
 
+	/// <summary>
+	/// キーの押下をチェック
+	/// </summary>
+	/// <param name="keyNumber">キー番号( DIK_0 等)</param>
+	/// <retuyrns>押されているか</returns>
+	bool PushKey(BYTE keyNumber);
+
+	/// <summary>
+	/// キーのトリガーをチェック
+	/// </summary>
+	/// <param name="keyNumber">キー番号( DIK_0 等)</param>
+	/// <retuyrns>トリガーか</returns>
+	bool TriggerKey(BYTE keyNumber);
+
 private://メンバ変数	
+    // DirectInputのインスタンス生成
+	ComPtr<IDirectInput8> dInput = nullptr;
+
 	//キーボードのデバイス
 	ComPtr<IDirectInputDevice8> keyboard;
+
+	//全キーの状態
+	BYTE key[256] = {};
+
+	//前回の全キーの状態
+	BYTE keyPre[256] = {};
 };
